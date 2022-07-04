4429a8f8a9493f01f11ab9da8eb0585d008e8d75
augusto <augusporres@gmail.com>
2022-07-04 19:05:45 -0300

    listo el calculo de m2

--- ca_room_sizing/scripts/gridToSqMeters.py
+++ ca_room_sizing/scripts/gridToSqMeters.py
@@ -5,35 +5,61 @@ import rospy
 import numpy as np
 from nav_msgs.msg import OccupancyGrid
 import matplotlib.pyplot as plt
-
+import sys
 
 def squareMeters():
-    #f = open('corridor_mapping_augusto.pgm', 'rb')
-    with open('corridor_mapping_augusto.pgm', 'rb') as f:
-        im = plt.imread(f)
-    plt.plot(im)
-    #print(f.readline())
-    #print(f.readline())
-    """(width, height) = [int(i) for i in f.readline().split()]
+    file1=sys.argv[1] + '.pgm'
+    file2=sys.argv[1] + '.yaml'
+    f = open(file1, 'rb')
+    f2 = open(file2, 'r')
+    f2.readline() # reads first line
+    resolution_field = f2.readline() #resolution is in 2nd line
+    resolution = resolution_field.split()[1]
+    print("Resolution", resolution)
+    header = f.readline()
+    f.readline() #author information
+    (width, height) = [int(i) for i in f.readline().split()]
     depth = int(f.readline())
+    assert depth <= 255
     raster = []
     for y in range(height):
         row = []
         for y in range(width):
             row.append(ord(f.read(1)))
         raster.append(row)
-    raster = np.array(raster)
-    toBinaryMatrix(raster)"""
     f.close()
+    raster = np.array(raster)
+    binary_grid = toBinaryMatrix(raster)
+    count = 0
+    for x in range(0,len(binary_grid[:,0])):
+        for y in range(0,len(binary_grid[0,:])):
+            if (binary_grid[x,y] == 1): # if probability > 205 set to 1, else 0
+                count +=1
+    __height = np.array([0]*len(binary_grid[:,0]))
+    __width = np.array([0]*len(binary_grid[0,:]))
+    for y in range(0, len(binary_grid[:,0])):
+        for x in range(0, len(binary_grid[0,:])):
+            if binary_grid[y,x] == 1:
+                __width[y] += 1
+    for x in range(0, len(binary_grid[0,:])):
+        for y in range(0, len(binary_grid[:,0])):
+            if binary_grid[x,y] == 1:
+                __height[y] += 1
+    m2 = __height.max() * __width.max() * float(resolution)**2
+    print("Ancho de celdas ocupadas: ", __width.max())
+    print("Alto de celdas ocupadas: ", __height.max())
+    print("Metros cuadrados: ", m2)
 
 def toBinaryMatrix(data):
-    for x in range(0,len(data)):
-        for y in range(0,len(data)):
-            if (data[x][y] > 50): # if probability > 50 set to 1, else 0
-                data[x][y] = 1
+    filtro = data[data[:,:] > 205]
+    for x in range(0,len(data[:,0])):
+        for y in range(0,len(data[0,:])):
+            if (data[x,y] > 205): # if probability > 205 set to 1, else 0
+                data[x,y] = 1
             else:
-                data[x][y] = 0
-    print(data)
+                data[x,y] = 0
+    print("OcGrid transformed succesfully to binary")
+    return data
     
 
 if __name__=='__main__':
